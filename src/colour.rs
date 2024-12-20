a8345c849604002d4d16caee24bb15cebe3e9b29
A_D <dragon@awesome-dragon.science>
2024-12-20 09:19:28 +0100

    add colour support

---
+++ src/colour.rs
@@ -0,0 +1,93 @@
+use std::ops::{Add, Div, Mul, Sub};
+
+use crate::math::{self, float::equal};
+
+#[derive(Clone, Copy, Debug)]
+pub struct Colour {
+    red: f64,
+    green: f64,
+    blue: f64,
+}
+
+macro_rules! impl_trait_simple {
+    ($trait:ident, $funcname:ident, $op:tt) => {
+        impl $trait for Colour {
+            type Output = Colour;
+            fn $funcname(self, rhs: Self) -> Self::Output {
+                Self {
+                    red: self.red $op rhs.red,
+                    green: self.green $op rhs.green,
+                    blue: self.blue $op rhs.blue,
+                }
+            }
+        }
+    };
+}
+
+impl_trait_simple!(Add, add, +);
+impl_trait_simple!(Sub, sub, -);
+impl_trait_simple!(Mul, mul, *);
+impl_trait_simple!(Div, div, /);
+impl Colour {
+    pub fn new(red: f64, green: f64, blue: f64) -> Self {
+        Self { red, green, blue }
+    }
+
+    pub fn newi(red: i32, green: i32, blue: i32) -> Self {
+        Self::new(red as f64, green as f64, blue as f64)
+    }
+}
+
+impl Mul<f64> for Colour {
+    type Output = Self;
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            red: self.red * rhs,
+            green: self.green * rhs,
+            blue: self.blue * rhs,
+        }
+    }
+}
+impl Mul<i32> for Colour {
+    type Output = Self;
+    fn mul(self, rhs: i32) -> Self::Output {
+        self * rhs as f64
+    }
+}
+impl PartialEq for Colour {
+    fn eq(&self, other: &Self) -> bool {
+        equal(self.red, other.red) && equal(self.green, other.green) && equal(self.blue, other.blue)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Colour;
+
+    #[test]
+    fn colours_work() {
+        let c = Colour {
+            red: 1.0,
+            green: 2.0,
+            blue: 3.0,
+        };
+
+        assert_eq!(c.red, 1.0);
+        assert_eq!(c.green, 2.0);
+        assert_eq!(c.blue, 3.0);
+    }
+
+    #[test]
+    fn simple_ops() {
+        let left = Colour::new(0.9, 0.6, 0.75);
+        let right = Colour::new(0.7, 0.1, 0.25);
+
+        assert_eq!(left + right, Colour::new(1.6, 0.7, 1.0));
+        assert_eq!(left - right, Colour::new(0.2, 00.5, 0.5));
+    }
+
+    #[test]
+    fn mul_scalar() {
+        assert_eq!(Colour::new(0.2, 0.3, 0.4) * 2, Colour::new(0.4, 0.6, 0.8));
+    }
+}
