24d097d16dd3968da134da654a1a19116cff094e
A_D <dragon@awesome-dragon.science>
2025-02-04 21:34:03 +0200

    implement precomputations for intersections

--- src/intersection.rs
+++ src/intersection.rs
@@ -1,9 +1,40 @@
-use crate::shape::Shape;
+use crate::{math::tuple::Tuple, ray::Ray, shape::Shape};
 
 #[derive(Clone, Copy, Debug)]
 pub struct Intersection<'a> {
     pub t: f64,
     pub object: &'a dyn Shape,
+}
+
+pub struct IntersectionComputions<'a> {
+    pub object: &'a dyn Shape,
+    pub t: f64,
+    pub point: Tuple,
+    pub eye_vector: Tuple,
+    pub normal_vector: Tuple,
+    pub inside: bool,
+}
+
+impl<'a> Intersection<'a> {
+    pub fn prepare_computations(&self, ray: Ray) -> IntersectionComputions<'a> {
+        let point = ray.position(self.t);
+        let normal_vector = self.object.normal_at(point);
+        let eye_vector = -ray.direction;
+        let inside = normal_vector.dot(&eye_vector) < 0.0;
+
+        IntersectionComputions {
+            object: self.object,
+            t: self.t,
+            point,
+            eye_vector,
+            normal_vector: if inside {
+                -normal_vector
+            } else {
+                normal_vector
+            },
+            inside,
+        }
+    }
 }
 
 impl<'a> Intersection<'a> {
@@ -80,5 +111,51 @@ mod test {
         let xs = vec![i1, i2, i3, i4];
 
         assert_eq!(xs.hit().expect("should exist"), i4)
+    }
+
+    mod computations {
+        use crate::math::tuple::{pointi, vectori};
+
+        use super::*;
+
+        #[test]
+        fn precompute() {
+            let r = Ray::new(pointi(0, 0, -5), vectori(0, 0, 1));
+            let shape = Sphere::new();
+            let intersection = Intersection::new(4.0, &shape);
+
+            let comps = intersection.prepare_computations(r);
+
+            assert_eq!(comps.object, &shape);
+            assert_eq!(comps.t, intersection.t);
+            assert_eq!(comps.point, pointi(0, 0, -1));
+            assert_eq!(comps.eye_vector, vectori(0, 0, -1));
+            assert_eq!(comps.normal_vector, vectori(0, 0, -1));
+        }
+
+        #[test]
+        fn precompute_outside() {
+            let ray = Ray::new(pointi(0, 0, -5), vectori(0, 0, 1));
+            let s = &Sphere::new();
+            let i = Intersection::new(4.0, s);
+
+            let comps = i.prepare_computations(ray);
+
+            assert!(!comps.inside);
+        }
+
+        #[test]
+        fn precompute_inside() {
+            let ray = Ray::new(pointi(0, 0, 0), vectori(0, 0, 1));
+            let s = &Sphere::new();
+            let i = Intersection::new(1.0, s);
+
+            let comps = i.prepare_computations(ray);
+
+            assert_eq!(comps.point, pointi(0, 0, 1));
+            assert_eq!(comps.eye_vector, vectori(0, 0, -1));
+            assert_eq!(comps.normal_vector, vectori(0, 0, -1)); // inverted you say?
+            assert!(comps.inside);
+        }
     }
 }
